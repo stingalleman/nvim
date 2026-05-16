7faed31d73dc37cc49eceae71109c83ab5789aa0
Sting Alleman <s.alleman@nfi.nl>
2026-05-16 23:50:57 +0200

    dap debugger

---
+++ lua/custom/plugins/dap.lua
@@ -0,0 +1,9 @@
+return {
+  'rcarriga/nvim-dap-ui',
+  dependencies = {
+    'mfussenegger/nvim-dap',
+    'nvim-neotest/nvim-nio',
+    'theHamsta/nvim-dap-virtual-text',
+    'nvim-telescope/telescope-dap.nvim',
+  },
+}
